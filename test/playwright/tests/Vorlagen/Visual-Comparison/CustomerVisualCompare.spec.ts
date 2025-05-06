a636510ce8028bb918ae754705330678cf69bf4a
Manuel Schnabel <mschnabel1992@gmail.com>
2025-05-06 23:38:04 +0200

    [TASK] Edit visual comparison tests and update URL list

--- test/playwright/tests/Vorlagen/Visual-Comparison/CustomerVisualCompare.spec.ts
+++ test/playwright/tests/Vorlagen/Visual-Comparison/CustomerVisualCompare.spec.ts
@@ -1,22 +1,32 @@
 import { test, expect } from '@playwright/test';
 import { urls }  from "./Urls";
 
+// let baseUrl = 'https://www.example.de/';
+// let baseUrl = 'https://staging.example.de/';
+let baseUrl = 'https://example.ddev.site/';
 
-const baseUrl = 'https://autodudes-blog.ddev.site/';
-
-test('<Customer> Screen Comparison', async ({ page }) => {
-
-    if (baseUrl === 'https://staging.example.com/') {
-        // login to htaccess
-        await page.goto('https://user:password@staging.example.com/');
-    }
+test('Visual Comparison example.de', async ({ page }) => {
     // max time, how long the test can run. Depends on the amount of URLs and pagespeed.
     test.setTimeout(10000000);
+
+    if (baseUrl === 'https://www.example.de/') {
+        await page.goto('https://www.example.de/');
+        await page.click('button[data-testid="uc-accept-all-button"]'); // click the cookie button of Usercentrics
+    }
+
+    if (baseUrl === 'https://user:password@staging.example.de/') {
+        await page.goto('https://user:password@staging.example.de/');
+    }
+
+    if (baseUrl === 'https://example.ddev.site/') {
+        await page.goto('https://example.ddev.site/');
+    }
+
     for (const url of urls) {
         try {
             await page.goto(baseUrl + url, { waitUntil: 'domcontentloaded' });
-            // wait for loading
-            await page.waitForTimeout(500);
+            await expect(page.locator('body')).not.toHaveText('Whoops, looks like something went wrong.');
+            await expect(page.locator('body')).not.toHaveText('Oops, an error occurred');
             await expect(page).toHaveScreenshot({ maxDiffPixels: 100, fullPage: true, timeout: 15000 });
         } catch (error) {
             console.log(error.message);
--- test/playwright/tests/Vorlagen/Visual-Comparison/Urls.ts
+++ test/playwright/tests/Vorlagen/Visual-Comparison/Urls.ts
@@ -1,10 +1,7 @@
 export const urls: string[] = [
-    'home/agencies',
-    'home/project-managers',
-    'freelancer',
-    'home/agencies',
-    'datenschutz',
-    'impressum',
+    '',
+    'kontakt'
+    // add more URLs to test
 ];
 
 
